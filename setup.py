695834d8d2c4e666467eb9f1e376d009d5de9646
Matt Graham <matthew.m.graham@gmail.com>
2022-03-29 00:01:49 +0100

    Pnned versions with environment markers in setup script

--- setup.py
+++ setup.py
@@ -2,8 +2,41 @@ from setuptools import setup, find_packages
 import pathlib
 
 here = pathlib.Path(__file__).parent.resolve()
-
 long_description = (here / 'README.md').read_text(encoding='utf-8')
+
+install_requires = [
+    "h5py==3.5.0",
+    "matplotlib==3.4.3",
+    "monai==0.7.0",
+    "nibabel==3.2.1"
+    "numpy==1.21.3",
+    "Pillow==8.4.0",
+    "scipy==1.7.1",
+    "scikit-image==0.18.3",
+    "tensorboard==2.7.0",
+    "torchio==0.18.57",
+    "tqdm==4.62.3",
+]
+
+CUDA_VERSION = "113"
+
+for os, machine in (('linux', 'x86_64'), ('win', 'amd64')):
+    for python_version in ('3.7', '3.8', '3.9'):
+        base_url = f'https://download.pytorch.org/whl/cu{CUDA_VERSION}'
+        abi = f'c{python_version[0]}{python_version[-1]}'
+        abi_2 = abi if python_version == '3.7' else f'{abi}m'
+        local_version_label = f'cu{CUDA_VERSION}-{abi}-{abi_2}-{os}_{machine}'
+        environment_marker = (
+            f'implementation_name == "cpython" '
+            f'and python_version == "{python_version}.*" '
+            f'and sys_platform == "{os}" '
+            f'and platform_machine == "{machine}"'
+        )
+        for package, package_version in (('torch', '1.10.0'), ('torchvision', '0.11.1')):
+            install_requires.append(
+                f'{package} @ {base_url}/{package}-{package_version}%2B'
+                f'{local_version_label}.whl ; {environment_marker}'
+            )
 
 setup(
     name='verydeepvae',
@@ -24,22 +57,17 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Environment :: GPU :: NVIDIA CUDA :: 11.3',
+        'Environment :: GPU :: NVIDIA CUDA :: 11.4',
+        'Environment :: GPU :: NVIDIA CUDA :: 11.5',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: Microsoft :: Windows',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
     ],
     keywords='pytorch, variational autoencoder, 3d',
+    license_files=('LICENSE',),
     packages=find_packages(),
     python_requires='>=3.7, <3.10',
-    install_requires=[
-        'h5py',
-        'matplotlib',
-        'monai',
-        'nibabel',
-        'numpy',
-        'pillow',
-        'scipy',
-        'scikit-image',
-        'torch',
-        'torchio',
-        'torchvision',
-        'tqdm',
-    ],
+    install_requires=install_requires,
 )
