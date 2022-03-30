490cddf973d02a82f26b84a7aea05cd20878cdec
Matt Graham <matthew.m.graham@gmail.com>
2022-03-30 11:34:45 +0100

    Fixes for Windows requirements

--- setup.py
+++ setup.py
@@ -5,33 +5,34 @@ here = pathlib.Path(__file__).parent.resolve()
 long_description = (here / 'README.md').read_text(encoding='utf-8')
 
 install_requires = [
-    "h5py==3.5.0",
-    "matplotlib==3.4.3",
-    "monai==0.7.0",
-    "nibabel==3.2.1",
-    "numpy==1.21.3",
-    "Pillow==8.4.0",
-    "scipy==1.7.1",
-    "scikit-image==0.18.3",
-    "tensorboard==2.7.0",
-    "torchio==0.18.57",
-    "tqdm==4.62.3",
+    'h5py==3.5.0',
+    'matplotlib==3.4.3',
+    'monai==0.7.0',
+    'nibabel==3.2.1',
+    'numpy==1.21.3',
+    'Pillow==8.4.0',
+    'scipy==1.7.1',
+    'scikit-image==0.18.3',
+    'tensorboard==2.7.0',
+    'torchio==0.18.57',
+    'tqdm==4.62.3',
 ]
 
 # CUDA builds of torch & torchvision packages are required therefore we install directly
 # from platform dependent pre-built wheels available at https://download.pytorch.org
 # These are only available for 64-bit Linux and Windows systems
-CUDA_VERSION = "113"
-for os, machine in (('linux', 'x86_64'), ('win', 'amd64')):
+CUDA_VERSION = '113'  # CUDA Toolkit version to get wheels for _without_ decimal point
+for os, machine in (('linux', 'x86_64'), ('win', 'AMD64')):
     for python_version in ('3.7', '3.8', '3.9'):
         base_url = f'https://download.pytorch.org/whl/cu{CUDA_VERSION}'
         abi = f'cp{python_version[0]}{python_version[-1]}'
         abi_2 = abi if python_version != '3.7' else f'{abi}m'
-        local_version_label = f'cu{CUDA_VERSION}-{abi}-{abi_2}-{os}_{machine}'
+        local_version_label = f'cu{CUDA_VERSION}-{abi}-{abi_2}-{os}_{machine.lower()}'
+        sys_platform = 'win32' if os == 'win' else os
         environment_marker = (
             f'implementation_name == "cpython" '
             f'and python_version == "{python_version}.*" '
-            f'and sys_platform == "{os}" '
+            f'and sys_platform == "{sys_platform}" '
             f'and platform_machine == "{machine}"'
         )
         for package, package_version in (('torch', '1.10.0'), ('torchvision', '0.11.1')):
