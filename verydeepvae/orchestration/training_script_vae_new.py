d9e232b374162ee502d4c0f24cd5b75b832bf2bf
Matt Graham <matthew.m.graham@gmail.com>
2022-05-25 15:47:01 +0100

    Only close writer object on rank 0 process

--- verydeepvae/orchestration/training_script_vae_new.py
+++ verydeepvae/orchestration/training_script_vae_new.py
@@ -934,4 +934,5 @@ def main(hyper_params):
                     # data_dictionary_1 is left over from the recon logic and is used each time we sample
                     del data_dictionary_1
 
-    writer.close()
+    if hyper_params['local_rank'] == 0:
+        writer.close()
