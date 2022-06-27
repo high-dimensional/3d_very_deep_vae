367ddadb4039b9533fa029b85a6635e9acf21bca
Matt Graham <matthew.m.graham@gmail.com>
2022-06-27 18:09:20 +0100

    Add JSON schema for model configuration files (#36)
    * Initial configuration file JSON schema
    
    * Remove deprecated properties and remaining descriptions
    
    * More removing and renaming options + adding max_niis_to_use
    
    * Update Linux requirements files
    
    * Updating Windows requirements files
    
    * Validating loaded configuration against schema
    
    * Validate example configurations against schema in checks
    
    * Use defaults for configuration options not explicitly set
    
    * Adding tests for invalid configuration files raising error
    
    * Add Markdown rendering of schema and link in README
    
    * Add tox environment for generating Markdown schema doc + add check for up to date
    
    * Add schema Markdown file to manifest
    
    * Remove unneeded brace character from JSON snippet

--- scripts/train_vae_model.py
+++ scripts/train_vae_model.py
@@ -5,6 +5,7 @@ import json
 import os
 from pathlib import Path
 from typing import Any, Dict
+import jsonschema
 import torch
 from verydeepvae.orchestration.training import train_model
 
@@ -128,6 +129,68 @@ def post_process_hyperparameters(
             )
 
 
+def load_config_schema():
+    schema_path = Path(__file__).parent.parent / "model_configuration.schema.json"
+    with open(schema_path, "r") as f:
+        try:
+            schema = json.load(f)
+        except json.JSONDecodeError as e:
+            raise json.JSONDecodeError(
+                f"Configuration schema file {schema_path} not valid JSON"
+            ) from e
+    return schema
+
+
+def extend_validator_with_default(validator_class):
+    """Extend jsonschema validator class to set optional property values to defaults.
+
+    Renaming of function provided in FAQ of jsonschema documentation at
+    https://github.com/python-jsonschema/jsonschema/blob/
+    642a09f08318605b16563f47073d3e7b73025029/docs/faq.rst
+
+    License for original code:
+
+    Copyright (c) 2013 Julian Berman
+
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
+
+    The above copyright notice and this permission notice shall be included in
+    all copies or substantial portions of the Software.
+
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+    THE SOFTWARE.
+    """
+    validate_properties = validator_class.VALIDATORS["properties"]
+
+    def set_defaults(validator, properties, instance, schema):
+        for property, subschema in properties.items():
+            if "default" in subschema:
+                instance.setdefault(property, subschema["default"])
+
+        for error in validate_properties(
+            validator,
+            properties,
+            instance,
+            schema,
+        ):
+            yield error
+
+    return jsonschema.validators.extend(
+        validator_class,
+        {"properties": set_defaults},
+    )
+
+
 def main():
     cli_args = parse_command_line_arguments()
     if not cli_args.json_config_file.exists():
@@ -145,6 +208,9 @@ def main():
             raise json.JSONDecodeError(
                 f"Configuration file {cli_args.json_config_file} not valid JSON"
             ) from e
+    schema = load_config_schema()
+    validator = extend_validator_with_default(jsonschema.Draft202012Validator)(schema)
+    validator.validate(hyperparameters, schema)
     post_process_hyperparameters(hyperparameters, cli_args)
     torch.multiprocessing.set_start_method("spawn")
     train_model(hyperparameters)
--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -89,40 +89,6 @@ def configuration_dict():
         "batch_size": 1,
         "resolution": 32,
         "max_niis_to_use": 2,
-        "plot_recons_period": 1,
-        "subjects_to_plot": 4,
-        "validation_period": 1,
-        "save_period": 1,
-        "warmup_iterations": 50,
-        "gradient_clipping_value": 100,
-        "gradient_skipping_value": 1000000000000,
-        "l2_reg_coeff": 1e-4,
-        "learning_rate": 1e-3,
-        "train_frac": 0.95,
-        "half_precision": False,
-        "output_activation_function": "tanh",
-        "plot_gradient_norms": True,
-        "convolutional_downsampling": False,
-        "bottleneck_resnet_encoder": True,
-        "only_use_one_conv_block_at_top": False,
-        "normalise_weight_by_depth": True,
-        "zero_biases": True,
-        "use_rezero": False,
-        "veto_batch_norm": True,
-        "veto_transformations": False,
-        "apply_augmentations_to_validation_set": False,
-        "visualise_training_pipeline_before_starting": True,
-        "resume_from_checkpoint": False,
-        "restore_optimiser": True,
-        "keep_every_checkpoint": True,
-        "predict_x_scale": True,
-        "predict_x_scale_with_sigmoid": True,
-        "use_precision_reweighting": False,
-        "separate_hidden_loc_scale_convs": False,
-        "separate_output_loc_scale_convs": False,
-        "verbose": True,
-        "scale_hidden_clamp_bounds": [0.001, 1],
-        "scale_output_clamp_bounds": [0.01, 1],
         "latent_feature_maps_per_resolution": [2, 4, 4, 3, 2, 1],
         "channels_per_latent": [10, 10, 10, 10, 10, 50],
         "channels": [10, 20, 30, 40, 50, 60],
@@ -131,10 +97,42 @@ def configuration_dict():
         "channels_hidden": [10, 20, 30, 40, 50, 60],
         "channels_top_down": [10, 20, 30, 40, 50, 60],
         "channels_hidden_top_down": [10, 20, 30, 40, 50, 60],
-        "latent_feature_maps_per_resolution_weight_sharing": "none",
-        "latents_to_use": "all",
-        "latents_to_optimise": "all",
     }
+
+
+@pytest.fixture(scope="module")
+def synthetic_data_dir(tmp_path_factory):
+    voxels_per_axis = 32
+    number_of_files = 2
+    data_dir = tmp_path_factory.mktemp("data")
+    generate_data_args = [
+        "--voxels_per_axis",
+        str(voxels_per_axis),
+        "--number_of_files",
+        str(number_of_files),
+        "--output_directory",
+        str(data_dir),
+        "--random_seed",
+        str(SEED),
+    ]
+    run_script("generate_synthetic_data.py", generate_data_args, check_return_code=True)
+    return data_dir
+
+
+def train_vae_model_with_configuration(configuration_dict, data_dir, tmp_path_factory):
+    configuration_path = tmp_path_factory.mktemp("configurations") / "test_config.json"
+    with open(configuration_path, "w") as f:
+        json.dump(configuration_dict, f)
+    outputs_dir = tmp_path_factory.mktemp("outputs")
+    train_model_args = [
+        "--json_config_file",
+        str(configuration_path),
+        "--nifti_dir",
+        str(data_dir),
+        "--output_dir",
+        str(outputs_dir),
+    ]
+    run_script("train_vae_model.py", train_model_args, check_return_code=True)
 
 
 @pytest.mark.parametrize(
@@ -163,33 +161,31 @@ def configuration_dict():
     ids=lambda d: ",".join(f"{k}={v}" for k, v in d.items()) if d else "base",
 )
 def test_generate_synthetic_data_and_train_vae_model(
-    tmp_path_factory, configuration_dict, configuration_updates
+    tmp_path_factory, synthetic_data_dir, configuration_dict, configuration_updates
 ):
-    voxels_per_axis = 32
-    number_of_files = 2
-    data_dir = tmp_path_factory.mktemp("data")
-    generate_data_args = [
-        "--voxels_per_axis",
-        str(voxels_per_axis),
-        "--number_of_files",
-        str(number_of_files),
-        "--output_directory",
-        str(data_dir),
-        "--random_seed",
-        str(SEED),
-    ]
-    run_script("generate_synthetic_data.py", generate_data_args, check_return_code=True)
     configuration_dict.update(configuration_updates)
-    configuration_path = tmp_path_factory.mktemp("configurations") / "test_config.json"
-    with open(configuration_path, "w") as f:
-        json.dump(configuration_dict, f)
-    outputs_dir = tmp_path_factory.mktemp("outputs")
-    train_model_args = [
-        "--json_config_file",
-        str(configuration_path),
-        "--nifti_dir",
-        str(data_dir),
-        "--output_dir",
-        str(outputs_dir),
-    ]
-    run_script("train_vae_model.py", train_model_args, check_return_code=True)
+    train_vae_model_with_configuration(
+        configuration_dict, synthetic_data_dir, tmp_path_factory
+    )
+
+
+@pytest.mark.parametrize(
+    "invalid_configuration_updates",
+    [
+        {"total_epochs": -1},
+        {"batch_size": "100.2"},
+        {"foo": True},
+    ],
+    ids=lambda d: ",".join(f"{k}={v}" for k, v in d.items()) if d else "base",
+)
+def test_generate_synthetic_data_and_train_vae_model_with_invalid_config_file_raises(
+    tmp_path_factory,
+    synthetic_data_dir,
+    configuration_dict,
+    invalid_configuration_updates,
+):
+    configuration_dict.update(invalid_configuration_updates)
+    with pytest.raises(AssertionError, match="jsonschema.exceptions.ValidationError"):
+        train_vae_model_with_configuration(
+            configuration_dict, synthetic_data_dir, tmp_path_factory
+        )
